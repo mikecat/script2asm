ab980f5b3847e3e2371899b10271e0cae4fc2f31
MikeCAT <mikecat@users.noreply.github.com>
2014-02-22 18:44:21 +0900

    added standalone expr2tree (for testing)

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 CXX      = g++
-OBJS     = expr2tree.o read_one_line.o script2asm.o data_type.o identifier_info.o
-BIN      = expr2tree.exe
+OBJS     = expr2tree.o read_one_line.o data_type.o identifier_info.o
+BINOBJS  = script2asm.o expr2tree_sa.o
+BIN      = script2asm.exe expr2tree_sa.exe
 CXXFLAGS = -O2 -std=c++98 -Wall -Wextra -Werror-implicit-function-declaration -finput-charset=cp932 -fexec-charset=cp932
 LDFLAGS  = -s -static
 LIBS     = 
@@ -10,8 +11,11 @@ RM       = rm -f
 
 all: $(BIN)
 
-expr2tree.exe: $(OBJS)
-	$(CXX) $(LDFLAGS) -o expr2tree.exe $(OBJS) $(LIBS)
+script2asm.exe: $(OBJS) script2asm.o
+	$(CXX) $(LDFLAGS) -o script2asm.exe $(OBJS) script2asm.o $(LIBS)
+
+expr2tree_sa.exe: $(OBJS) expr2tree_sa.o
+	$(CXX) $(LDFLAGS) -o expr2tree_sa.exe $(OBJS) expr2tree_sa.o $(LIBS)
 
 expr2tree.o: expr2tree.cpp
 	$(CXX) $(CXXFLAGS) -c -o expr2tree.o expr2tree.cpp
@@ -28,5 +32,8 @@ data_type.o: data_type.cpp
 identifier_info.o: identifier_info.cpp
 	$(CXX) $(CXXFLAGS) -c -o identifier_info.o identifier_info.cpp
 
+expr2tree_sa.o: expr2tree_sa.cpp
+	$(CXX) $(CXXFLAGS) -c -o expr2tree_sa.o expr2tree_sa.cpp
+
 clean:
-	$(RM) $(BIN) $(OBJS)
+	$(RM) $(BIN) $(OBJS) $(BINOBJS)
---
+++ expr2tree_sa.cpp
@@ -0,0 +1,127 @@
+/*
+Copyright 2014 MikeCAT.
+
+This software is provided "as is", without any express or implied warranties,
+including but not limited to the implied warranties of merchantability and
+fitness for a particular purpose.  In no event will the authors or contributors
+be held liable for any direct, indirect, incidental, special, exemplary, or
+consequential damages however caused and on any theory of liability, whether in
+contract, strict liability, or tort (including negligence or otherwise),
+arising in any way out of the use of this software, even if advised of the
+possibility of such damage.
+
+Permission is granted to anyone to use this software for any purpose, including
+commercial applications, and to alter and distribute it freely in any form,
+provided that the following conditions are met:
+
+1. The origin of this software must not be misrepresented; you must not claim
+   that you wrote the original software. If you use this software in a product,
+   an acknowledgment in the product documentation would be appreciated but is
+   not required.
+
+2. Altered source versions may not be misrepresented as being the original
+   software, and neither the name of MikeCAT nor the names of
+   authors or contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+3. This notice must be included, unaltered, with any source distribution.
+*/
+
+#include <cstdio>
+#include "expr2tree.h"
+
+using namespace Expr2tree;
+
+static std::map<OperatorType,std::string> op2str;
+static std::map<IdentifierType,std::string> id2str;
+
+void initPrint(void) {
+	op2str[OP_INVALID]="invalid";
+	op2str[OP_IDENTIFIER]="identifier";
+	op2str[OP_CALL_FUNCTION]="call_function";
+	op2str[OP_ARRAY]="[]";
+	op2str[OP_INC_AFTER]="++(after)";
+	op2str[OP_INC_BEFORE]="++(before)";
+	op2str[OP_DEC_AFTER]="++(after)";
+	op2str[OP_DEC_BEFORE]="++(before)";
+	op2str[OP_READADDR]="* (addr)";
+	op2str[OP_GETADDR]="& (addr)";
+	op2str[OP_PLUS]="+ (sign)";
+	op2str[OP_MINUS]="- (sign)";
+	op2str[OP_BIT_NOT]="~";
+	op2str[OP_LOGIC_NOT]="!";
+	op2str[OP_MUL]="*";
+	op2str[OP_DIV]="/";
+	op2str[OP_MOD]="%";
+	op2str[OP_ADD]="+";
+	op2str[OP_SUB]="-";
+	op2str[OP_LSHIFT]="<<";
+	op2str[OP_RSHIFT]=">>";
+	op2str[OP_SMALLER]="<";
+	op2str[OP_BIGGER]=">";
+	op2str[OP_SMALLER_EQUAL]="<=";
+	op2str[OP_BIGGER_EQUAL]=">=";
+	op2str[OP_EQUAL]="=";
+	op2str[OP_NOT_EQUAL]="!=";
+	op2str[OP_BIT_AND]="&";
+	op2str[OP_BIT_XOR]="^";
+	op2str[OP_BIT_OR]="|";
+	op2str[OP_LOGIC_AND]="&&";
+	op2str[OP_LOGIC_OR]="||";
+	op2str[OP_ASSIGN]="=";
+	op2str[OP_MUL_ASSIGN]="*=";
+	op2str[OP_DIV_ASSIGN]="/=";
+	op2str[OP_MOD_ASSIGN]="%=";
+	op2str[OP_ADD_ASSIGN]="+=";
+	op2str[OP_SUB_ASSIGN]="-=";
+	op2str[OP_LSHIFT_ASSIGN]="<<=";
+	op2str[OP_RSHIFT_ASSIGN]=">>=";
+	op2str[OP_AND_ASSIGN]="&=";
+	op2str[OP_XOR_ASSIGN]="^=";
+	op2str[OP_OR_ASSIGN]="|=";
+	op2str[OP_CONNECT]=",";
+
+	id2str[IDENTIFIER_FUNCTION]="関数";
+	id2str[IDENTIFIER_GLOBAL_VALUE]="グローバル変数";
+	id2str[IDENTIFIER_LOCAL_VALUE]="ローカル変数";
+	id2str[IDENTIFIER_INTEGER_LITERAL]="整数リテラル";
+	id2str[IDENTIFIER_STRING_LITERAL]="文字列リテラル";
+}
+
+void printExprList(const ExprList& exl,int level=0) {
+	for(ExprList::const_iterator it=exl.begin();it!=exl.end();it++) {
+		const ExprNode& en=**it;
+		for(int i=0;i<level;i++)fputs("  ",stdout);
+		printf("operator %d (%s)",(int)en.opType,op2str[en.opType].c_str());
+		if(en.opType==OP_IDENTIFIER) {
+			printf(" %s",id2str[en.idInfo.getType()].c_str());
+			if(en.idInfo.getType()==IDENTIFIER_FUNCTION ||
+			en.idInfo.getType()==IDENTIFIER_GLOBAL_VALUE) {
+				printf(" %s",en.idInfo.getName().c_str());
+			} else if(en.idInfo.getType()==IDENTIFIER_STRING_LITERAL) {
+				printf(" \"%s\"",en.idInfo.getName().c_str());
+			} else {
+				printf(" %d",en.idInfo.getValue());
+			}
+		}
+		putchar('\n');
+		printExprList(en.childNodes,level+1);
+	}
+}
+
+int main(void) {
+	char buffer[10000];
+	IdentifierMap imap;
+	initPrint();
+	while(fgets(buffer,sizeof(buffer),stdin)) {
+		ExprList exl;
+		ErrorType err;
+		exl=expr2tree(err,std::string(buffer),imap);
+		if(err==SUCCESS) {
+			printExprList(exl);
+		} else {
+			printf("Error %d\n",(int)err);
+		}
+	}
+	return 0;
+}
