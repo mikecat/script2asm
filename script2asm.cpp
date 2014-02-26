a03ac932f958b42b657bb07b719b5ad3b78f5934
MikeCAT <mikecat@users.noreply.github.com>
2014-02-26 12:07:42 +0900

    made raw-assembly mode to ignore any keywords but endfunction

--- script2asm.cpp
+++ script2asm.cpp
@@ -65,140 +65,146 @@ void Script2asm::workWithOneLine(const std::string& rawLine) {
 	stringPair keywordAndValue=divideKeywordAndValue(now);
 	const std::string& keyword=keywordAndValue.first;
 	const std::string& value=keywordAndValue.second;
-	// まず、複数行コメントの処理を行う
-	if(keyword=="comment") {
-		if(value!="") {
-			printWarning(std::string("stray \"")+value+"\" ignored");
-		}
-		commentCounter++;
-	} else if(keyword=="endcomment") {
-		if(commentCounter==0) {
-			throwError("stray \"endcomment\"");
-		}
-		if(value!="") {
-			printWarning(std::string("stray \"")+value+"\" ignored");
-		}
-		commentCounter--;
-	} else if(commentCounter<=0) {
-		// 複数行コメントの中ではない時のみ、処理を行う
-		if(keyword=="global") {
-			if(status!=STATUS_TOP) {
-				throwError("stray \"global\"");
-			}
-			if(value!="") {
-				printWarning(std::string("stray \"")+value+"\" ignored");
-			}
-			status=STATUS_GLOBAL_VARIABLE;
-		} else if(keyword=="endglobal") {
-			if(status!=STATUS_GLOBAL_VARIABLE) {
-				throwError("stray \"endglobal\"");
-			}
-			if(value!="") {
-				printWarning(std::string("stray \"")+value+"\" ignored");
-			}
+	if(status==STATUS_FUNCTION_ASSEMBLY) {
+		// 生のアセンブリの出力(endfunction以外の一切のキーワードを無視する)
+		if(keyword=="endfunction") {
 			status=STATUS_TOP;
-		} else if(keyword=="function") {
-			// ステータスを確認
-			if(status!=STATUS_TOP) {
-				throwError("stray \"function\"");
-			}
-			// 関数名と戻り値の型に分ける
-			stringPair functionNameAndType=divideKeywordAndValue(value);
-			const std::string& functionName=functionNameAndType.first;
-			const std::string& functionType=functionNameAndType.second;
-			if(globalFunctionAndVariableList.count(functionName)!=0) {
-				// 既にその関数が存在する: 分割宣言の実装とみなす
-				if(globalFunctionAndVariableList.at(functionName).
-				getIdentifierType()!=IDENTIFIER_FUNCTION) {
-					throwError(functionName+" is already defined and not a function");
-				} else if(functionType!="") {
-					printWarning("return type written here is ignored");
-				}
-				needCommitToList=false;
-			} else {
-				// 新規関数
-				if(functionType=="") {
-					throwError("return type is required");
-				}
-				needCommitToList=true;
-			}
-			// 関数のパラメータの初期化
-			nowFunctionName=functionName;
-			nowFunctionReturnType=parseType(functionType);
-			nowFunctionParameterTypes.clear();
-			nowFunctionLocalVariableList.clear();
-			parameterOffset=4;
-			localVariableOffset=0;
-			status=STATUS_FUNCTION_TOP;
-		} else if(keyword=="parameters") {
-		} else if(keyword=="variables") {
-		} else if(keyword=="procedure") {
-		} else if(keyword=="assembly") {
-		} else if(keyword=="endfunction") {
-		} else if(keyword=="if") {
-		} else if(keyword=="elseif") {
-		} else if(keyword=="else") {
-		} else if(keyword=="endif") {
-		} else if(keyword=="while") {
-		} else if(keyword=="wend") {
-		} else if(keyword=="do") {
-		} else if(keyword=="dowhile") {
-		} else if(keyword=="repeat") {
-		} else if(keyword=="loop") {
-		} else if(keyword=="continue") {
-		} else if(keyword=="break") {
-		} else if(keyword=="return") {
 		} else {
-			switch(status) {
-				case STATUS_TOP:
-				case STATUS_FUNCTION_TOP:
-					// 不正
-					throwError("Invalid expression");
-					break;
-				case STATUS_GLOBAL_VARIABLE:
-					// グローバル変数の宣言
-					if(globalFunctionAndVariableList.count(keyword)!=0) {
-						throwError(keyword+" is already defined");
+			fputs((rawLine+"\n").c_str(),outputFile);
+		}
+	} else {
+		// まず、複数行コメントの処理を行う
+		if(keyword=="comment") {
+			if(value!="") {
+				printWarning(std::string("stray \"")+value+"\" ignored");
+			}
+			commentCounter++;
+		} else if(keyword=="endcomment") {
+			if(commentCounter==0) {
+				throwError("stray \"endcomment\"");
+			}
+			if(value!="") {
+				printWarning(std::string("stray \"")+value+"\" ignored");
+			}
+			commentCounter--;
+		} else if(commentCounter<=0) {
+			// 複数行コメントの中ではない時のみ、処理を行う
+			if(keyword=="global") {
+				if(status!=STATUS_TOP) {
+					throwError("stray \"global\"");
+				}
+				if(value!="") {
+					printWarning(std::string("stray \"")+value+"\" ignored");
+				}
+				status=STATUS_GLOBAL_VARIABLE;
+			} else if(keyword=="endglobal") {
+				if(status!=STATUS_GLOBAL_VARIABLE) {
+					throwError("stray \"endglobal\"");
+				}
+				if(value!="") {
+					printWarning(std::string("stray \"")+value+"\" ignored");
+				}
+				status=STATUS_TOP;
+			} else if(keyword=="function") {
+				// ステータスを確認
+				if(status!=STATUS_TOP) {
+					throwError("stray \"function\"");
+				}
+				// 関数名と戻り値の型に分ける
+				stringPair functionNameAndType=divideKeywordAndValue(value);
+				const std::string& functionName=functionNameAndType.first;
+				const std::string& functionType=functionNameAndType.second;
+				if(globalFunctionAndVariableList.count(functionName)!=0) {
+					// 既にその関数が存在する: 分割宣言の実装とみなす
+					if(globalFunctionAndVariableList.at(functionName).
+					getIdentifierType()!=IDENTIFIER_FUNCTION) {
+						throwError(functionName+" is already defined and not a function");
+					} else if(functionType!="") {
+						printWarning("return type written here is ignored");
 					}
-					globalFunctionAndVariableList[keyword]=
-						IdentifierInfo::makeGlobalVariable(
-							keyword,parseType(value)
-						);
-					break;
-				case STATUS_FUNCTION_PARAMETERS: // 仮引数の宣言
-				case STATUS_FUNCTION_VARIABLES: // ローカル変数の宣言
-					{
-						// 変数名が被っていないかチェックする
-						if(nowFunctionLocalVariableList.count(keyword)!=0) {
+					needCommitToList=false;
+				} else {
+					// 新規関数
+					if(functionType=="") {
+						throwError("return type is required");
+					}
+					needCommitToList=true;
+				}
+				// 関数のパラメータの初期化
+				nowFunctionName=functionName;
+				nowFunctionReturnType=parseType(functionType);
+				nowFunctionParameterTypes.clear();
+				nowFunctionLocalVariableList.clear();
+				parameterOffset=4;
+				localVariableOffset=0;
+				status=STATUS_FUNCTION_TOP;
+			} else if(keyword=="parameters") {
+			} else if(keyword=="variables") {
+			} else if(keyword=="procedure") {
+			} else if(keyword=="assembly") {
+			} else if(keyword=="endfunction") {
+			} else if(keyword=="if") {
+			} else if(keyword=="elseif") {
+			} else if(keyword=="else") {
+			} else if(keyword=="endif") {
+			} else if(keyword=="while") {
+			} else if(keyword=="wend") {
+			} else if(keyword=="do") {
+			} else if(keyword=="dowhile") {
+			} else if(keyword=="repeat") {
+			} else if(keyword=="loop") {
+			} else if(keyword=="continue") {
+			} else if(keyword=="break") {
+			} else if(keyword=="return") {
+			} else {
+				switch(status) {
+					case STATUS_TOP:
+					case STATUS_FUNCTION_TOP:
+					case STATUS_FUNCTION_ASSEMBLY: // avoid warning
+						// 不正
+						throwError("Invalid expression");
+						break;
+					case STATUS_GLOBAL_VARIABLE:
+						// グローバル変数の宣言
+						if(globalFunctionAndVariableList.count(keyword)!=0) {
 							throwError(keyword+" is already defined");
 						}
-						int nowOffset=0;
-						DataType nowType=parseType(value);
-						// オフセットを計算する
-						if(status==STATUS_FUNCTION_PARAMETERS) {
-							nowOffset=parameterOffset;
-							parameterOffset+=nowType.getTypeSize();
-							// ついでに仮引数リストに加える
-							nowFunctionParameterTypes.push_back(nowType);
-						} else {
-							localVariableOffset-=nowType.getTypeSize();
-							nowOffset=localVariableOffset;
-						}
-						// ローカル変数のリストに加える
-						nowFunctionLocalVariableList[keyword]=
-							IdentifierInfo::makeLocalVariable(
-								nowOffset,nowType
+						globalFunctionAndVariableList[keyword]=
+							IdentifierInfo::makeGlobalVariable(
+								keyword,parseType(value)
 							);
-					}
-					break;
-				case STATUS_FUNCTION_PROCEDURE:
-					// 実際の計算処理
-					// not impremented yet
-					break;
-				case STATUS_FUNCTION_ASSEMBLY:
-					// 生のアセンブリの出力
-					fputs((rawLine+"\n").c_str(),outputFile);
-					break;
+						break;
+					case STATUS_FUNCTION_PARAMETERS: // 仮引数の宣言
+					case STATUS_FUNCTION_VARIABLES: // ローカル変数の宣言
+						{
+							// 変数名が被っていないかチェックする
+							if(nowFunctionLocalVariableList.count(keyword)!=0) {
+								throwError(keyword+" is already defined");
+							}
+							int nowOffset=0;
+							DataType nowType=parseType(value);
+							// オフセットを計算する
+							if(status==STATUS_FUNCTION_PARAMETERS) {
+								nowOffset=parameterOffset;
+								parameterOffset+=nowType.getTypeSize();
+								// ついでに仮引数リストに加える
+								nowFunctionParameterTypes.push_back(nowType);
+							} else {
+								localVariableOffset-=nowType.getTypeSize();
+								nowOffset=localVariableOffset;
+							}
+							// ローカル変数のリストに加える
+							nowFunctionLocalVariableList[keyword]=
+								IdentifierInfo::makeLocalVariable(
+									nowOffset,nowType
+								);
+						}
+						break;
+					case STATUS_FUNCTION_PROCEDURE:
+						// 実際の計算処理
+						// not impremented yet
+						break;
+				}
 			}
 		}
 	}
